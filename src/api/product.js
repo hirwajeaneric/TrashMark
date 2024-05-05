e6ab3a33e3bf2c289b65d2187a40a5aaa13429ff
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-05-05 18:15:22 +0200

    Feature: All product APIs

--- src/api/product.js
+++ src/api/product.js
@@ -41,3 +41,121 @@ export const AddProductRequest = async (data) => {
     return responseData;
 };
 
+export const updateProductRequest = async (data) => {
+
+    const formData = new FormData();
+
+    for (const key in data) {
+        formData.append(key, data[key]);
+    }
+
+    // If imageFiles is an array of files
+    if (data.imageFiles) {
+        for (const file of data.imageFiles) {
+            formData.append('imageFiles', file);
+        }
+    }
+
+    const response = await fetch(`${API_BASE_URL}/api/v1/product/update`, {
+        method: 'PUT',
+        headers: {
+            'Authorization': `Bearer ${Cookies.get('access-token')}`,
+        },
+        body: formData,
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+        if (responseData.errors) {
+            throw new Error(responseData.errors);
+        }
+        if (responseData.message) {
+            throw new Error(responseData.message);
+        }
+    }
+
+    return responseData;
+};
+
+export const getUserProductsRequest = async () => {
+    const response = await fetch(`${API_BASE_URL}/api/v1/product/user`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${Cookies.get('access-token')}`,
+        },
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+        if (responseData.errors) {
+            throw new Error(responseData.errors);
+        }
+        if (responseData.message) {
+            throw new Error(responseData.message);
+        }
+    }
+
+    return responseData;
+};
+
+export const getProductByIdRequest = async (id) => {
+    const response = await fetch(`${API_BASE_URL}/api/v1/product/findById?id=${id}`, {
+        method: 'GET'
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+        if (responseData.errors) {
+            throw new Error(responseData.errors);
+        }
+        if (responseData.message) {
+            throw new Error(responseData.message);
+        }
+    }
+
+    return responseData;
+}
+
+export const getAllAvailableProductsRequest = async () => {
+    const response = await fetch(`${API_BASE_URL}/api/v1/product/available`, {
+        method: 'GET'
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+        if (responseData.errors) {
+            throw new Error(responseData.errors);
+        }
+        if (responseData.message) {
+            throw new Error(responseData.message);
+        }
+    }
+
+    return responseData;
+};
+
+export const getBaughtProductsRequest = async () => {
+    const response = await fetch(`${API_BASE_URL}/api/v1/product/baught`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${Cookies.get('access-token')}`,
+        },
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+        if (responseData.errors) {
+            throw new Error(responseData.errors);
+        }
+        if (responseData.message) {
+            throw new Error(responseData.message);
+        }
+    }
+
+    return responseData;
+}
